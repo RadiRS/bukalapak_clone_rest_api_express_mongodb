b5d5640c562ee10263380bb4da3d1f0dada1feb4
Radi <radi.rusadi.s@gmail.com>
2019-02-19 03:10:24 +0700

    Adding JWT Token user login

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://admin_01:admin_01@cluster0-jdmtp.mongodb.net/bukalapak_clone_db?retryWrites=true'
+    'mongodb+srv://admin_01:admin_01@cluster0-jdmtp.mongodb.net/bukalapak_clone_db?retryWrites=true',
+  secretOrKey: 'thisisthesecretkey'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User model
 const User = require('../../models/User');
@@ -63,7 +65,23 @@ router.post('/login', (req, res) => {
     // Check for password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: 'Success' });
+        // User match
+
+        // Create jwt payload
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer' + token
+            });
+          }
+        );
       } else {
         return res.status(404).json({ password: 'Password incorrect' });
       }
