4284a39f8b733ca536db6c64328aa05de1319521
Radi <radi.rusadi.s@gmail.com>
2019-02-19 02:54:02 +0700

    Adding user login

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://admin_01:admin_01@cluster0-jdmtp.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://admin_01:admin_01@cluster0-jdmtp.mongodb.net/bukalapak_clone_db?retryWrites=true'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -46,4 +46,29 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route   GET api/v1/users/login
+// @desc    Login users route / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // Check for password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(404).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
+
 module.exports = router;
